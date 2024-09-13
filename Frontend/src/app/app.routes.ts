6bb60220fcef93af4ddfb075346389c56177ff7e
Jennifer Springer <j.springer@bbs-fm.de>
2024-09-13 19:59:47 +0200

    fun

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegistrationComponent } from './authentication/registration/registration.component';
+import { HomeComponent } from './base-components/home/home.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegistrationComponent },
     { path: '**', redirectTo: '' }
