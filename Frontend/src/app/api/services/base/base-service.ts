2cb9b17df8b09bcd6c158338ece04ff4826827e5
Jennifer Springer <j.springer@bbs-fm.de>
2024-09-27 17:02:24 +0200

    environment

--- Frontend/src/app/api/services/base/base-service.ts
+++ Frontend/src/app/api/services/base/base-service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "../../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export abstract class BaseService {
-    protected url = 'https://localhost:44383/api/';
+    protected url = environment.backend + 'api/';
 
     constructor(
         protected _httpClient: HttpClient) {
---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  backend: 'https://js.api.kata.my-bbs.net/'
+};
